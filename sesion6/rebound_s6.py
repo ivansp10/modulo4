82664fb5ada944693d7c6ac6bc1ad95f249e1d4e
ivansp <ivan.sp100@gmail.com>
2023-05-30 18:59:09 -0400

    finalizacion rebound sesion 6

---
+++ sesion6/rebound_s6.py
@@ -0,0 +1,10 @@
+## rebound sesion 6##
+
+suma = 3000
+contador = 0
+
+try:
+    resultado = suma/contador
+    print("Resultado", resultado)
+except ZeroDivisionError:
+    print("División por cero")
